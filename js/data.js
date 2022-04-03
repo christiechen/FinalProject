fffec421d4b2f5225d0b5875ea0b4b17e4e0daef
christiechen <christiec@wustl.edu>
2022-04-02 23:58:06 -0500

    more functions added, example zoom data hierarchy commented out in code

--- js/data.js
+++ js/data.js
@@ -4,6 +4,7 @@ $(function() {
         
 
         //get numbers to be numbers
+        //remove the "(1)" from area titles
         data.forEach((el)=>{
             for(let propt in el){
                 if(propt !== "State" && propt !== "Area"){
@@ -11,8 +12,15 @@ $(function() {
                 }
                
             }
+            if(el.Area.indexOf("(") !== -1){
+                el.Area = el.Area.substring(0, el.Area.indexOf("("));
+            }
         })
-        let origData = [...data];
+
+        let origData = JSON.parse(JSON.stringify(data));
+        console.log(origData);
+
+
         let industries = new Set();
 
         //split industry and year
@@ -150,10 +158,41 @@ $(function() {
         console.log(dataByIndustry);
 
 
-        console.log(getStateByYear(dataByYear, 2018));
-        console.log(getIndustryForYear(dataByIndustry, "Mining and Logging", 2018));
-        console.log(getIndustryForYearInState(dataByIndustry, "Mining and Logging", 2018, "Alaska"));
-    })
+        // console.log(getStateByYear(dataByYear, 2018));
+        // console.log(getIndustryForYear(dataByIndustry, "Mining and Logging", 2018));
+        // console.log(getIndustryForYearInState(dataByIndustry, "Mining and Logging", 2018, "Alaska"));
+        // console.log(getEmploymentForYear(dataByYear, 2018));
+
+
+        //EXAMPLE
+        console.log("EXAMPLE ZOOM DATA");
+
+        // example of a zoom:
+        
+        //level 1: all states and their total employment in a given year
+        console.log(getStateByYear(dataByYear, 2018))
+        
+        //zooming into Alabama 
+        //level 2: Alabama information – total employment in each major area (city
+        console.log(getCityTotalsForStateByYear(dataByState, "Alabama", 2018));
+
+        //zooming into a specific area for specific industries
+        // level 3: Alabama major areas — employment for each industry
+        console.log(getCitySpecificsByYear(dataByState, "Alabama", 2018, "Anniston-Oxford-Jacksonville"));
+
+
+
+        // HERE IS WHERE YOU WOULD CREATE VISUALIZATIONS.
+        // When the time comes, I'll move the functions into a separate JS file that we can include
+        // before all the rest of the JS files in the html doc so that there's no "function dne" issues.
+        // Ideally, you'll pass in a few of the datasets created above and then just use functions inside each object
+        // to generate the data that you need for each level.
+
+
+
+
+
+    });
 });
 
 
@@ -192,6 +231,73 @@ function getStateByYear(data, year){
 }
 
 
+//get an array of all TOTALS in a GIVEN STATE's cities in a given year
+// data: dataByState
+//output:
+// [
+//     {
+//         State: _____,
+//         Area: _____,
+//         TotalEmployees: _____
+//     }, ... 
+// ]
+
+function getCityTotalsForStateByYear(data, state,  yearFilter){
+    let workingData = data.get(state);
+    let ret = [];
+    for (let props in workingData){
+        let year = workingData[props][yearFilter]; 
+        for(let i = 0; i< year.length; i++){
+            if(year[i].Industry === 'Total'){
+                let obj = {
+                    State: state,
+                    Area: props,
+                    TotalEmployees: year[i].Employees
+                };
+                ret.push(obj);
+                break;
+            }
+        }
+    }
+
+    return ret;
+}
+
+//get an array of employment for all industries in a given area in a given state for a given year.
+// data: dataByState
+//output: 
+// [
+//     {
+//         State: ____, 
+//         Area: _____, 
+//         Industry: ____,
+//         Employees: ____,
+//     }, ...
+// ]
+function getCitySpecificsByYear(data, state, yearFilter, area){
+    let workingData = data.get(state);
+    workingData = workingData[area][yearFilter];
+    let ret = [];
+
+    workingData.forEach((el)=>{
+        //skip the "total" measure
+        if(el.Industry === 'Total'){
+            return false;
+        }
+
+        let obj = {
+            State: state,
+            Area: area,
+            Industry: el.Industry,
+            Employees: el.Employees
+        }
+        ret.push(obj);
+    })
+
+    return ret;
+}
+
+
 //get an array of employment for a given industry in a given year.
 // data: the dataByIndustry Map
 //output: 
@@ -221,10 +327,41 @@ function getIndustryForYear(data, industry, year){
 }
 
 
-//get an array of employment for a given industry in a given year FOR A GIVEN STATE
+//get an array of employment for a given industry in a given year FOR A GIVEN STATE. Separated by AREA
+// data: the dataByIndustry Map
+// output
+// [
+//     {
+//         State: ____, 
+//         Area: _____, 
+//         Employees: ____,
+//     }, ...
+// ]
 function getIndustryForYearInState(data, industry, year, state){
     let workingData = getIndustryForYear(data, industry, year);
     let ret = workingData.filter((el)=> el.State === state);
     return ret;
 
+}
+
+// get an array of employment for all areas in a given year. NOT separated by State
+//data: dataByYear
+function getEmploymentForYear(data, year){
+    let workingData = data.get(year);
+    let ret = [];
+    for (let [state, areas] of workingData){
+        areas.forEach((areaEmp)=>{
+            areaEmp.Employment.forEach((indEmp)=>{
+                let obj = {
+                    State: state,
+                    Area: areaEmp.Area,
+                    Employment: indEmp.Employees,
+                    Industry: indEmp.Industry
+                }
+
+                ret.push(obj);
+            })
+        })
+    }
+    return ret;
 }
